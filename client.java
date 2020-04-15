313a8bd5a167577462e9a55eef1c414535401683
ishannadeep <22831322+ishannadeep@users.noreply.github.com>
2020-04-15 18:52:24 +0530

    java client
    java application split the image and then encrypt it using AES hybrid encryption algorithm and send it the server as a data stream

---
+++ client.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reading_eye;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Base64;
+import javax.imageio.ImageIO;
+import static reading_eye.segment_Img.imageToBufferedImage;
+
+/**
+ *
+ * @author ishan
+ */
+public class Reading_eye {
+ public static final String REMOTE_HOST = "192.168.10.13";
+ public static final int REMOTE_PORT = 65480;
+    /**
+     * @param args the command line arguments
+     */
+    private static String encodeFileToBase64Binary(File file) throws Exception{
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            byte[] bytes = new byte[(int)file.length()];
+            fileInputStreamReader.read(bytes);
+            String encodedString=Base64.getEncoder().encodeToString(bytes);
+            return encodedString;
+    }
+    public static void main(String[] args) throws IOException {
+  
+     Socket socket = new Socket(REMOTE_HOST, REMOTE_PORT);
+  
+     Aes AES=new Aes();
+     //256-bit key and plain text
+     String key = "ahdfsujeytsbsdfawskdfhsdgfereijd";
+     //converting key and plain text to byte arrays
+     byte key_arr[] = key.getBytes("UTF8");
+     //expand key
+     int[][] keyarray= AES._expand_key(key_arr);
+     //encrypting plaintext
+
+        //Provide number of rows and column
+        int row = 5;
+        int col = 5;
+        BufferedImage originalImgage = ImageIO.read(new File("C:\\Users\\ishan\\Documents\\NetBeansProjects\\opencv_test_2\\src\\opencv_test_2\\book_2.jpg"));
+        //resizing the image into 1000*1000 pixels
+        Image newImage = originalImgage.getScaledInstance(1000, 1000, Image.SCALE_DEFAULT);
+        BufferedImage originalImgage_2 = imageToBufferedImage(newImage);
+        //total width and total height of an imageaas
+        int tWidth = originalImgage_2.getWidth();
+        int tHeight = originalImgage_2.getHeight();
+        //width and height of each piece
+        int eWidth = tWidth / col;
+        int eHeight = tHeight / row;
+        int x = 0;
+        int y = 0;
+        int round =0;
+        for (int i = 0; i < row; i++) {
+            y = 0;
+            for (int j = 0; j < col; j++) {
+                try {
+                    
+                    DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+                    DataInputStream din=new DataInputStream(socket.getInputStream());
+                    
+                    
+                    BufferedImage SubImgage = originalImgage_2.getSubimage(y, x, eWidth, eHeight);
+                    File outputfile = new File("C:\\Users\\ishan\\Documents\\NetBeansProjects\\opencv_test_2\\src\\opencv_test_2\\1000pixel"+i+j+".jpg");
+                    ImageIO.write(SubImgage, "jpg", outputfile);
+                    
+                    //String gh="helloaishannadeepkalansooriya";
+                    String encodstring = encodeFileToBase64Binary(outputfile);
+                    byte text_arr[] = encodstring.getBytes("UTF8");
+                    byte[] cipher=AES.encrypt(text_arr);
+
+                    dOut.write(cipher);
+
+                    round++;
+                    y += eWidth;
+                    //receving server acknowledgement
+                    byte receive= din.readByte();
+                  
+                    //53 is the byte format of servers response
+                    if(receive==53){
+                      //empting input and output data stream  
+                      dOut=null;
+                      din=null;
+                      continue;
+                    }else{
+                      break;
+                    }
+   
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            x += eHeight;
+        }   
+    }
+}
