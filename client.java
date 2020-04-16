25f22eb2763d0c857a9c468540c8296309fcb4b6
ishannadeep <22831322+ishannadeep@users.noreply.github.com>
2020-04-16 20:55:59 +0530

    Adding diffie hellman key exchange

--- client.java
+++ client.java
@@ -6,11 +6,15 @@
 package reading_eye;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Base64;
 import javax.imageio.ImageIO;
@@ -22,7 +26,9 @@ import static reading_eye.segment_Img.imageToBufferedImage;
  */
 public class Reading_eye {
  public static final String REMOTE_HOST = "192.168.10.13";
- public static final int REMOTE_PORT = 65480;
+ public static final int REMOTE_PORT = 65495;
+ 
+ long private_key = 48;
     /**
      * @param args the command line arguments
      */
@@ -33,18 +39,49 @@ public class Reading_eye {
             String encodedString=Base64.getEncoder().encodeToString(bytes);
             return encodedString;
     }
+    private static long power( long a, long b, long P) 
+    {  
+        if (b == 1) 
+            return a; 
+        else
+            return (a^ b) % P; 
+    } 
     public static void main(String[] args) throws IOException {
-  
-     Socket socket = new Socket(REMOTE_HOST, REMOTE_PORT);
-  
-     Aes_hybrid AES=new Aes_hybrid();
-     //256-bit key and plain text
-     String key = "ahdfsujeytsbsdfawskdfhsdgfereijd";
-     //converting key and plain text to byte arrays
-     byte key_arr[] = key.getBytes("UTF8");
-     //expand key
-     int[][] keyarray= AES._expand_key(key_arr);
-     //encrypting plaintext
+    //P and G are random numbers for diffie hellman key exchange
+    long P=230;
+    long G=99;
+    Socket socket = new Socket(REMOTE_HOST, REMOTE_PORT);
+    
+    BufferedWriter sockOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    BufferedReader sockIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+     // Write the text to the remote process.
+    //creating client generated key
+    String gen_key =""+power(G, 48, P);
+      //sending client generated key to the server
+      sockOut.write(gen_key, 0, gen_key.length());
+      sockOut.newLine();
+      sockOut.flush();
+ 
+      // Read the generated key of the server from the remote process.
+      String response = sockIn.readLine();
+
+      if (response == null) {
+        System.out.println("server didn't get the key");
+       
+      }else
+      {
+        long server_gen_key = Integer.parseInt(response);
+        //generating the secret key
+        String sec_key=""+(server_gen_key^48)%P;
+
+         Aes_hybrid AES=new Aes_hybrid();
+         //Adding generated secret key to the key to get 256-bit key 
+        String key = sec_key+"gdbtiomagRDbsHLkdfhsdgfereijd";
+        //converting key and plain text to byte arrays
+        byte key_arr[] = key.getBytes("UTF8");
+        //expand key
+        int[][] keyarray= AES._expand_key(key_arr);
+        //encrypting plaintext
 
         //Provide number of rows and column
         int row = 5;
@@ -102,6 +139,7 @@ public class Reading_eye {
                 }
             }
             x += eHeight;
-        }   
+        }
+      }   
     }
 }
